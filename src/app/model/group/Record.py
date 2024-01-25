195bb5c3ab5c37070548cf9bdc26472cfd5e6037
Adam Jennings <ajennings@tier2consulting.com>
2024-01-25 17:18:43 +0000

    [patch] Remove Redundant comments

--- src/app/model/group/Record.py
+++ src/app/model/group/Record.py
@@ -2,12 +2,7 @@ from src.app.model.base import Base
 
 
 class Record(Base):
-    """ Tracked player information """
+    """ Tracked Record for the Historic Streaks """
     name = ''
     streak = 0
     date = '0/0/0000'
-
-    # def __init__(self, name, streak, date):
-    #     self.name = name
-    #     self.streak = streak
-    #     self.date = date
