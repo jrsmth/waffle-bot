5cd68780feea395584282a85253e0427678e7423
jrsmth-tier2 <jsmith@tier2consulting.com>
2024-02-16 10:23:56 +0000

    Fix quotation mark err

--- src/app/archbishop/archbishop_spec.py
+++ src/app/archbishop/archbishop_spec.py
@@ -3,13 +3,13 @@ import pytest
 
 @pytest.fixture
 def fake_envs(monkeypatch):
-    monkeypatch.setenv('REDIS_TOKEN', "REDIS_TOKEN")
-    monkeypatch.setenv('REDIS_URL', "redis://REDIS_URL")
-    monkeypatch.setenv('SLACK_BOT_TOKEN', "BOT_TOKEN")
-    monkeypatch.setenv('SLACK_SIGNING_SECRET', "SLACK_SECRET")
-    monkeypatch.setenv('SLACK_SCOPES', "[SCOPE_ONE, SCOPE_2]")
-    monkeypatch.setenv('DEBUG', "False")
-    monkeypatch.setenv('PORT', "3000")
+    monkeypatch.setenv('REDIS_TOKEN', 'REDIS_TOKEN')
+    monkeypatch.setenv('REDIS_URL', 'redis://REDIS_URL')
+    monkeypatch.setenv('SLACK_BOT_TOKEN', 'BOT_TOKEN')
+    monkeypatch.setenv('SLACK_SIGNING_SECRET', 'SLACK_SECRET')
+    monkeypatch.setenv('SLACK_SCOPES', '["SCOPE_ONE", "SCOPE_2"]')
+    monkeypatch.setenv('DEBUG', 'False')
+    monkeypatch.setenv('PORT', '3000')
 
 
 @pytest.fixture()
@@ -24,6 +24,6 @@ def test_hello(fake_envs, app):
     # order of fixtures is important
 
     test_client = app.test_client()
-    response = test_client.get("/")
+    response = test_client.get('/')
 
     assert response.status_code == 200
