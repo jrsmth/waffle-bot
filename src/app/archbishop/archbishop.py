496f05d20c857f29f36cf98a2fc373b03f142ab9
Adam Jennings <ajennings@tier2consulting.com>
2024-03-19 09:48:35 +0000

    [#41] Respond to PR comments. Process the average score locally

--- src/app/archbishop/archbishop.py
+++ src/app/archbishop/archbishop.py
@@ -64,7 +64,7 @@ def construct_blueprint(bolt, config, messages, redis):
         player = get_player(event, group)
 
         log.debug(f"[handle_waffle] Updating player information for [{player.name}]")
-        player.score = handle_avg_score(event.get_score(), player)
+        player.score += event.get_score()
         player.streak = event.get_streak()
         player.games += 1
 
@@ -79,12 +79,9 @@ def construct_blueprint(bolt, config, messages, redis):
         say(response)
         return Response(messages.load("event.request.handled"), status=200)
 
-    def handle_avg_score(event_score, player):
-        player.played += 1
-        player.score = (player.score + event_score) / player.played
-
+    def get_avg_score(player):
         # Return average score to 2 decimal places
-        return round(player.score, 2)
+        return round(player.score / player.games, 2)
 
     def get_group(event):
         """ Fetch group object from redis """
