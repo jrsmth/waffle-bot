aa40ddf9b62bb7e707b3bf5eaa0953104b3e2c99
Adam Jennings <ajennings@tier2consulting.com>
2024-03-19 15:31:19 +0000

    [#42] User containing unique ID

--- src/app/model/group/player.py
+++ src/app/model/group/player.py
@@ -6,6 +6,7 @@ from src.app.model.base import Base
 @dataclass
 class Player(Base):
     """ Tracked player information """
+    id: str
     name: str
     streak: int
     score: int
@@ -13,6 +14,7 @@ class Player(Base):
     @classmethod
     def from_dict(cls, dic):
         return cls(
+            id=dic["id"],
             name=dic["name"],
             streak=dic["streak"],
             score=dic["score"]
