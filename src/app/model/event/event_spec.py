cd4d1f9869683eb735963a7d53bf7ec14f38c687
Adam Jennings <ajennings@tier2consulting.com>
2024-05-22 10:29:10 +0100

    [#61 / bugfix] Allow reading score of X/5 - when a streak is over

--- src/app/model/event/event_spec.py
+++ src/app/model/event/event_spec.py
@@ -3,79 +3,41 @@ from from_root import from_root
 from src.app.model.event.event import Event
 
 
+def extract_and_validate(file_name, expected_score, expected_streak):
+    with open(from_root("resources", "test", "event_samples", file_name)) as event_file:
+        event_dict = json.load(event_file)
+
+    event = Event(event_dict)
+
+    actual_score = event.get_score()
+    actual_streak = event.get_streak()
+
+    assert expected_score == actual_score
+    assert expected_streak == actual_streak
+
+
 class EventSpec:
 
     def should_extract_when_waffle_is_only_text_present(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_nothing_else.json")) as event_file:
-            event_dict = json.load(event_file)
-
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 0 == actual_score
-        assert 4 == actual_streak
+        extract_and_validate("event_waffle_score_nothing_else.json", 0, 4)
 
     def should_extract_when_waffle_is_before_short_text(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_short_text_after.json")) as event_file:
-            event_dict = json.load(event_file)
-
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 0 == actual_score
-        assert 4 == actual_streak
+        extract_and_validate( "event_waffle_score_short_text_after.json", 0, 4)
 
     def should_extract_when_waffle_is_after_short_text(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_short_text_before.json")) as event_file:
-            event_dict = json.load(event_file)
-
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 0 == actual_score
-        assert 4 == actual_streak
+        extract_and_validate("event_waffle_score_short_text_before.json", 0, 4)
 
     def should_extract_when_waffle_is_before_long_text(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_long_text_after.json")) as event_file:
-            event_dict = json.load(event_file)
-
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 3 == actual_score
-        assert 6 == actual_streak
+        extract_and_validate("event_waffle_score_long_text_after.json", 3, 6)
 
     def should_extract_when_waffle_is_after_long_text(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_long_text_before.json")) as event_file:
-            event_dict = json.load(event_file)
-
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 3 == actual_score
-        assert 6 == actual_streak
+        extract_and_validate("event_waffle_score_long_text_before.json", 3, 6)
 
     def should_extract_when_waffle_is_after_false_flags(self):
-        with open(from_root("resources", "test", "event_samples", "event_waffle_score_after_fakes.json")) as event_file:
-            event_dict = json.load(event_file)
+        extract_and_validate("event_waffle_score_after_fakes.json", 0, 4)
 
-        event = Event(event_dict)
-
-        actual_score = event.get_score()
-        actual_streak = event.get_streak()
-
-        assert 0 == actual_score
-        assert 4 == actual_streak
+    def should_extract_when_waffle_is_broken_streak(self):
+        extract_and_validate("event_waffle_streak_over.json", 0, 0)
 
     def should_not_extract_from_invalid_post(self):
         with open(from_root("resources", "test", "event_samples", "event_keyword_match_no_score.json")) as event_file:
